38750c5ec9fc6215be51327b37ec2b6bcab7c25c
AmGSircan <cantaytv@gmail.com>
2020-12-26 14:34:58 +0700

    List products cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Button } from 'react-bootstrap';
+import formatCurrency from '../util';
+import ListProductsInCart from './ListProductsInCart';
 
 export default class Cart extends Component {
     constructor(props){
         super(props);
+        this.removeFromCart = this.removeFromCart.bind(this);
+    }
+
+    removeFromCart(product){
+        this.props.removeFromCart(product)
     }
 
     render() {
@@ -19,6 +26,19 @@ export default class Cart extends Component {
                         <Navbar.Brand href="#home">You have {cartItems.length} in the cart</Navbar.Brand>
                     </Navbar>
                 }
+                {cartItems.map(item => <ListProductsInCart key={item._id} item={item} removeFromCart={this.removeFromCart} />)}
+                {cartItems.length !== 0 && (
+                        <>
+                            <div className="dropdown-divider"></div>
+                            Total: {" "}
+                            {formatCurrency(
+                                cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                            )}
+                            <Button variant="info">Proceed</Button>
+                        </>
+                    )
+                }
+                
             </div>
         )
     }
