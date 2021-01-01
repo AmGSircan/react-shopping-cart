457c89100aafe79d04eac5d51003b1c2f02661da
AmGSircan <cantaytv@gmail.com>
2021-01-01 18:12:22 +0700

    add redux to cart

--- src/App.js
+++ src/App.js
@@ -8,47 +8,6 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cartItems: localStorage.getItem("cartItems")
-        ? JSON.parse(localStorage.getItem("cartItems"))
-        : [],
-    };
-  }
-
-  createOrder = (order) => {
-    alert("Need to save order for " + order.name);
-  };
-
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((item) => item._id !== product._id),
-    });
-    localStorage.setItem(
-      "cartItems",
-      JSON.stringify(cartItems.filter((item) => item._id !== product._id))
-    );
-  };
-
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-
-    cartItems.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-      cartItems.push({ ...product, count: 1 });
-    }
-    this.setState({ cartItems });
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  };
-
   render() {
     return (
       <Provider store={store}>
@@ -62,15 +21,11 @@ class App extends React.Component {
           <Col md={9}>
             <Filter />
             <Row>
-              <Products addToCart={this.addToCart} />
+              <Products />
             </Row>
           </Col>
           <Col md={3}>
-            <Cart
-              createOrder={this.createOrder}
-              cartItems={this.state.cartItems}
-              removeFromCart={this.removeFromCart}
-            />
+            <Cart />
           </Col>
         </Row>
       </Provider>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,96 +1,141 @@
-import React, { Component } from 'react';
-import { Navbar, Button, Form } from 'react-bootstrap';
-import formatCurrency from '../util';
-import ListProductsInCart from './ListProductsInCart';
+import React, { Component } from "react";
+import { Navbar, Button, Form } from "react-bootstrap";
+import formatCurrency from "../util";
+import ListProductsInCart from "./ListProductsInCart";
 import { Fade } from "react-reveal";
+import { connect } from "react-redux";
+import { removeFromCart } from "../actions/cartAction";
 
-export default class Cart extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            showCheckout: false,
-            name: "",
-            email: "",
-            address: ""
-        }
-    }
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCheckout: false,
+      name: "",
+      email: "",
+      address: "",
+    };
+  }
 
-    removeFromCart = product => {
-        this.props.removeFromCart(product)
-    }
+  removeFromCart = (product) => {
+    this.props.removeFromCart(product);
+  };
 
-    handleInput = e => {
-        this.setState({[e.target.name]: e.target.value})
-    }
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    createOrder = e => {
-        e.preventDefault()
-        const order = {
-            name: this.state.name,
-            email: this.state.email,
-            address: this.state.address,
-            cartItems: this.props.cartItems
-        }
-        this.props.createOrder(order)
-    }
+  createOrder = (e) => {
+    e.preventDefault();
+    const order = {
+      name: this.state.name,
+      email: this.state.email,
+      address: this.state.address,
+      cartItems: this.props.cartItems,
+    };
+    this.props.createOrder(order);
+  };
 
-    render() {
-        const {cartItems} = this.props;
-        return (
-            <div>
-                {cartItems.length === 0 ?
-                    <Navbar bg="light">
-                        <Navbar.Brand href="#home">Cart is empty</Navbar.Brand>
-                    </Navbar> :
-                    <Navbar bg="light">
-                        <Navbar.Brand href="#home">You have {cartItems.length} in the cart</Navbar.Brand>
-                    </Navbar>
-                }
-                {cartItems.map(item => <ListProductsInCart key={item._id} item={item} removeFromCart={this.removeFromCart} />)}
-                {cartItems.length !== 0 && (
-                        <>
-                            <div className="dropdown-divider"></div>
-                            Total: {" "}
-                            {formatCurrency(
-                                cartItems.reduce((a, c) => a + c.price * c.count, 0)
-                            )}
-                            <Button onClick={() => this.setState({
-                                showCheckout: true
-                            })} variant="info">Proceed</Button>
-                        </>
-                    )
-                }
-                {this.state.showCheckout && (
-                    <>
-                        <div className="dropdown-divider"></div>
-                        <Fade bottom cascade>
-                            <Form>
-                                <Form.Group>
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control required name="email" type="email" placeholder="Enter email" onChange={this.handleInput} />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                    </Form.Text>
-                                </Form.Group>
+  render() {
+    const { cartItems } = this.props;
+    return (
+      <div>
+        {cartItems.length === 0 ? (
+          <Navbar bg="light">
+            <Navbar.Brand href="#home">Cart is empty</Navbar.Brand>
+          </Navbar>
+        ) : (
+          <Navbar bg="light">
+            <Navbar.Brand href="#home">
+              You have {cartItems.length} in the cart
+            </Navbar.Brand>
+          </Navbar>
+        )}
+        {cartItems.map((item) => (
+          <ListProductsInCart
+            key={item._id}
+            item={item}
+            removeFromCart={this.removeFromCart}
+          />
+        ))}
+        {cartItems.length !== 0 && (
+          <>
+            <div className="dropdown-divider"></div>
+            Total:{" "}
+            {formatCurrency(
+              cartItems.reduce((a, c) => a + c.price * c.count, 0)
+            )}
+            <Button
+              onClick={() =>
+                this.setState({
+                  showCheckout: true,
+                })
+              }
+              variant="info"
+            >
+              Proceed
+            </Button>
+          </>
+        )}
+        {this.state.showCheckout && (
+          <>
+            <div className="dropdown-divider"></div>
+            <Fade bottom cascade>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    required
+                    name="email"
+                    type="email"
+                    placeholder="Enter email"
+                    onChange={this.handleInput}
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-                                <Form.Group>
-                                    <Form.Label>Enter your name</Form.Label>
-                                    <Form.Control name="name" type="text" placeholder="Name" onChange={this.handleInput} />
-                                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Enter your name</Form.Label>
+                  <Form.Control
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={this.handleInput}
+                  />
+                </Form.Group>
 
-                                <Form.Group>
-                                    <Form.Label>Enter your address</Form.Label>
-                                    <Form.Control required name="address" type="text" placeholder="Address" onChange={this.handleInput} />
-                                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Enter your address</Form.Label>
+                  <Form.Control
+                    required
+                    name="address"
+                    type="text"
+                    placeholder="Address"
+                    onChange={this.handleInput}
+                  />
+                </Form.Group>
 
-                                <Button onClick={this.createOrder} variant="success" type="submit">
-                                    Checkout
-                                </Button>
-                            </Form>
-                        </Fade>
-                    </>
-                )}
-            </div>
-        )
-    }
+                <Button
+                  onClick={this.createOrder}
+                  variant="success"
+                  type="submit"
+                >
+                  Checkout
+                </Button>
+              </Form>
+            </Fade>
+          </>
+        )}
+      </div>
+    );
+  }
 }
+
+export default connect(
+  (state) => ({
+    cartItems: state.cart.cartItems,
+  }),
+  { removeFromCart }
+)(Cart);
